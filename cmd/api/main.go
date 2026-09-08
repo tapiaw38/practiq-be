5385a25a04317b6e0199cd83c01c0f3e7fe4212a
Walter Tapia <tapiaw38@gmail.com>
2026-09-08 16:39:34 -0300

    fix(cors): allow render landing

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -47,7 +47,7 @@ func main() {
 	app := gin.Default()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{cfg.ServerConfig.FrontendURL, "https://app.practiq.com.ar", "https://practiq.com.ar", "https://www.practiq.com.ar", "http://localhost:5174", "http://localhost:5173", "http://localhost:4321", "http://127.0.0.1:4321"},
+		AllowOrigins:     []string{cfg.ServerConfig.FrontendURL, "https://app.practiq.com.ar", "https://practiq.com.ar", "https://www.practiq.com.ar", "https://practiq-landing.onrender.com", "http://localhost:5174", "http://localhost:5173", "http://localhost:4321", "http://127.0.0.1:4321"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
